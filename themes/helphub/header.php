e8ba959b453395c72791a296dc0c5bbcab5179c0
carl alberto <cralberto11@gmail.com>
2016-02-04 22:02:26 +0800

    This pulls out the WP.org header - WPORGPATH is defined in my local wp-config to point to https://wordpress.org/header.php to make it work in your local

--- themes/helphub/header.php
+++ themes/helphub/header.php
@@ -25,20 +25,8 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'helphub' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+		<div class="wporg-header">
+			<?php require WPORGPATH . 'header.php';	?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
