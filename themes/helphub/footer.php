d65b0e47975d8eb72fb66be015f064146a2812d9
carl alberto <cralberto11@gmail.com>
2016-04-17 21:26:27 +0800

    Added 2 widget position for the footer

--- themes/helphub/footer.php
+++ themes/helphub/footer.php
@@ -12,22 +12,26 @@
 ?>
 
 		</div><!-- #content -->
-	</div><!-- #content wrapper -->	
+	</div><!-- #content wrapper -->
 	<div class="helphub-footerarea">
 		<div class="wrapper">
-			<h2 class="helphub-footerarea-title">Join the Conversation and Learn More</h2>
+			<h2 class="helphub-footerarea-title"><?php echo __( 'Join the Conversation and Learn More', 'helphub' ); ?></h2>
 			<div class="helphub-footerarea1">
-				<h3 class="helphub-footerarea-title">WordPress Forums</h3>
-				<div class="helphub-footerarea-body">Join the broader WordPress community via the online Forums. You can ask questions or offer answers to other users' questions.</div>
-				<button>Ask a question</button>
+				<?php
+					if ( is_active_sidebar( 'footer-1' ) ) {
+						get_template_part( 'template-parts/widget', 'footer1' );
+					}
+				?>
 			</div>
 			<div class="helphub-footerarea2">
-				<h3 class="helphub-footerarea-title">WordPress Documentation</h3>
-				<div class="helphub-footerarea-body">Read the WordPress documentation to get started with WordPress and complete quick tutorials.</div>
-				<button>View documentation</button>
+				<?php
+					if ( is_active_sidebar( 'footer-2' ) ) {
+						get_template_part( 'template-parts/widget', 'footer2' );
+					}
+				?>
 			</div>
-		</div>	
-	</div>			
+		</div>
+	</div>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php require WPORGPATH . 'footer.php'; ?>
 	</footer><!-- #colophon -->
