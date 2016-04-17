da10859692629ff5ce98c20442f225a859e661a4
carl alberto <cralberto11@gmail.com>
2016-04-17 19:17:36 +0800

    Replaced the autoloaded frontpage with a front page template

--- themes/helphub/template-frontpage.php
+++ themes/helphub/template-frontpage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The front page template file.
+ * Template Name: Frontpage
  *
  * If the user has selected a static page for their homepage, this is what will
  * appear. Learn more: http://codex.wordpress.org/Template_Hierarchy
@@ -16,7 +16,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="main-search-area">
-				<h1 class="site-main-title">HelpHub</h1>
+				<h1 class="site-main-title"><?php the_title(); ?></h1>
 				<!-- #search placeholder only, will be changed later on when search engine code is ready START-->
 				<div class="helphub-search">
 					<div class="helphub-search-box">
@@ -25,22 +25,28 @@ get_header(); ?>
 					<div class="helphub-search-btn">
 						<button>SEARCH</button>
 					</div>
-				</div>	
+				</div>
+				<div class="helphub-contentarea">
+					<?php
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'template-parts/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+
+					endwhile; // End of the loop.
+					?>
+				</div>
 				<!-- #search placeholder only, will be changed later on when search engine code is ready END-->
 			</div>
 			<div class="main-widget-area">
-				<?php for ($x=0; $x < 6; $x++) { ?> 
-					<div class="main-widget-box col-3">
-						<h3 class="main-widget-box-title">Installing Wordpress</h3>
-						<ul>
-							<li>Topic 1</li>
-							<li>Topic 2</li>
-							<li>Topic 3</li>
-							<li>Topic 4</li>
-							<li>Topic 5</li>
-						</ul>	
-					</div>
-				<?php } ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php endwhile; else : ?>
+<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
