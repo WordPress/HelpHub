d658b27f6647a4d2237c866767fc43378d6dfc07
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2016-12-23 08:56:26 +0900

    Add PHPUnit testing framework (#68)
    * add phpunit testing
    
    * add test files
    
    * add phpunit to .travis.yml
    
    * bug fix on travis

---
+++ tests/test-helphub-theme.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Oreore
+ */
+
+/**
+ * Sample test case.
+ */
+class Helphub_Theme_Test extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
