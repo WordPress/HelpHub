0261e1125e70feffb4b477d25d58a842468fb4c2
carl alberto <cralberto11@gmail.com>
2017-01-09 02:09:52 +0800

    Tried to elinminate  2 | ERROR | [ ] Missing short description in doc comment    |       |     (Generic.Commenting.DocComment.MissingShort)  9 | ERROR | [x] Additional blank lines found at end of doc comment    |       |     (Generic.Commenting.DocComment.SpacingAfter)

--- plugins/table-of-contents-lite/uninstall.php
+++ plugins/table-of-contents-lite/uninstall.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package WordPress
  * This file runs when the plugin in uninstalled (deleted).
  * This will not run when the plugin is deactivated.
  * Ideally you will add all your clean-up scripts here
  * that will clean-up unused meta, options, etc. in the database.
- *
+ * @package WordPress
+ * @author Carl Alberto
+ * @since 1.0.0
  */
-
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+if (! defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
