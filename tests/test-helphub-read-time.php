04ab5926884d47aad31546fef88780c683ad4629
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-01-11 00:02:22 +0900

    remove setUp.

--- tests/test-helphub-read-time.php
+++ tests/test-helphub-read-time.php
@@ -10,22 +10,15 @@
  */
 class Helphub_Read_Time_Test extends WP_UnitTestCase {
 
-
-	/**
-	 * Setup our test server, endpoints, and user info.
-	 */
-	public function setUp() {
-		parent::setUp();
-		$this->user = $this->factory->user->create( array(
-			'role' => 'administrator',
-		) );
-	}
-
 	/**
 	 * @covers hh_calculate_and_update_post_read_time
 	 */
 	public function test_hh_calculate_and_update_post_read_time() {
-		wp_set_current_user( $this->user );
+		$user = $this->factory->user->create( array(
+			'role' => 'administrator',
+		) );
+
+		wp_set_current_user( $user );
 
 		// 175 words.
 		$lorem_ipsum_for_minute = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget';
