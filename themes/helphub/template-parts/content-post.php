a3908e66880f5e4f35eb09ed720ae9674b9ebcdf
Mircea Sandu <mirceas17@gmail.com>
2017-06-15 15:24:31 +0200

    Copying basic styles from handbook

--- themes/helphub/template-parts/content-post.php
+++ themes/helphub/template-parts/content-post.php
@@ -10,7 +10,101 @@
 
 ?>
 <aside class="single-post-sidebar">
-	Test
+	<div class="sidebar-content">
+		<div class="widget widget_wporg_handbook_pages">
+			<h2 class="widget-title">Chapters</h2>
+			<div class="menu-table-of-contents-container">
+				<ul>
+					<li class="page_item page-item-5905 page_item_has_children current_page_ancestor current_page_parent current-menu-ancestor current-menu-parent menu-item menu-item-has-children open"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/about-the-docs-team/">About the docs team</a></div>
+						<ul class="children" style="display: none;">
+							<li class="page_item page-item-5922 current_page_item current-menu-item menu-item"><a href="https://make.wordpress.org/docs/handbook/about-the-docs-team/current-docs-projects/">Current Docs Projects</a></li>
+							<li class="page_item page-item-5924 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/about-the-docs-team/get-involved/">Get Involved</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-3646 menu-item"><a href="https://make.wordpress.org/docs/handbook/about-the-docs-team/get-involved/getting-started-at-a-contributor-day/">Getting Started at a Contributor Day</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li class="page_item page-item-227 menu-item"><a href="https://make.wordpress.org/docs/handbook/best-practice/">Best Practice</a></li>
+					<li class="page_item page-item-3889 menu-item"><a href="https://make.wordpress.org/docs/handbook/codex/">Codex</a></li>
+					<li class="page_item page-item-229 menu-item"><a href="https://make.wordpress.org/docs/handbook/credits/">Credits</a></li>
+					<li class="page_item page-item-5981 page_item_has_children menu-item menu-item-has-children"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/developer-resources/">Developer Resources</a></div>
+						<ul class="children" style="display: none;">
+							<li class="page_item page-item-5941 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/developer-resources/code-reference/">Code Reference</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-6791 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/developer-resources/code-reference/codex-migration/">Codex Migration</a>
+										<ul class="children default-open">
+											<li class="page_item page-item-6795 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/code-reference/codex-migration/for-contributors/">Migrating Examples For Contributors</a></li>
+											<li class="page_item page-item-6797 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/code-reference/codex-migration/reviewing-migrated-examples/">Reviewing Migrated Examples</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="page_item page-item-3860 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/devhub/">Devhub</a></li>
+							<li class="page_item page-item-3887 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/developer-resources/handbooks/">Handbooks</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-781 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/handbooks/handbooks-style-and-formatting-guide/">Handbooks Style and Formatting Guide</a></li>
+									<li class="page_item page-item-1456 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/handbooks/tutorial-template/">Tutorial Template</a></li>
+								</ul>
+							</li>
+							<li class="page_item page-item-6822 menu-item"><a href="https://make.wordpress.org/docs/handbook/developer-resources/tools/">Tools</a></li>
+						</ul>
+					</li>
+					<li class="page_item page-item-6916 page_item_has_children menu-item menu-item-has-children"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/helphub/">HelpHub</a></div>
+						<ul class="children">
+							<li class="page_item page-item-7141 menu-item"><a href="https://make.wordpress.org/docs/handbook/helphub/getting-started-with-helphub/">Getting Started with HelpHub</a></li>
+							<li class="page_item page-item-7136 menu-item"><a href="https://make.wordpress.org/docs/handbook/helphub/helphub-development/">HelpHub Development</a></li>
+							<li class="page_item page-item-7133 menu-item"><a href="https://make.wordpress.org/docs/handbook/helphub/migrating-articles/">Migrating Articles</a></li>
+						</ul>
+					</li>
+					<li class="page_item page-item-5945 page_item_has_children menu-item menu-item-has-children"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/core/">Intro to Core for Documentarians</a></div>
+						<ul class="children">
+							<li class="page_item page-item-4338 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/">Admin Help</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-4983 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/previous-user-test/">Previous User Test</a></li>
+									<li class="page_item page-item-4666 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/the-goal-of-the-admin-help-plugin-team/">The Goal of the Admin Help Plugin Team</a></li>
+									<li class="page_item page-item-4545 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/ui-pattern-gallery/">UI Pattern Gallery</a>
+										<ul class="children default-open">
+											<li class="page_item page-item-4991 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/ui-pattern-gallery/ui-pattern-gallery-squarespace/">UI Pattern Gallery (SquareSpace)</a></li>
+										</ul>
+									</li>
+									<li class="page_item page-item-5202 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/user-testing-scenario-for-irl-tests/">User testing scenario for IRL tests</a></li>
+									<li class="page_item page-item-4902 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/admin-help/user-testing-round-three-theme-test/">User Testing, Round Three – Theme Test</a></li>
+								</ul>
+							</li>
+							<li class="page_item page-item-3870 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/core/inline-docs/">Inline Docs</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-3878 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/inline-docs/closed-hooks-ticket-review/">Closed Hooks Ticket Review</a></li>
+									<li class="page_item page-item-3873 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/inline-docs/hook-docs-triage/">Hook Docs Triage</a></li>
+									<li class="page_item page-item-3881 menu-item"><a href="https://make.wordpress.org/docs/handbook/core/inline-docs/unclaimed-hook-docs-files/">Unclaimed Hook Docs Files</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li class="page_item page-item-161 page_item_has_children menu-item menu-item-has-children"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/references/">References</a></div>
+						<ul class="children">
+							<li class="page_item page-item-163 menu-item"><a href="https://make.wordpress.org/docs/handbook/references/writing-tips/">Writing Tips</a></li>
+						</ul>
+					</li>
+					<li class="page_item page-item-145 menu-item"><a href="https://make.wordpress.org/docs/handbook/requirements/">Requirements</a></li>
+					<li class="page_item page-item-1454 menu-item"><a href="https://make.wordpress.org/docs/handbook/templates/">Templates</a></li>
+					<li class="page_item page-item-147 menu-item"><a href="https://make.wordpress.org/docs/handbook/tools/">Tools</a></li>
+					<li class="page_item page-item-151 page_item_has_children menu-item menu-item-has-children"><div class="expandable"><span class="dashicons dashicons-arrow-down-alt2"></span><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/">Tutorials and Guides</a></div>
+						<ul class="children">
+							<li class="page_item page-item-153 page_item_has_children menu-item menu-item-has-children"><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/the-codex/">The Codex</a>
+								<ul class="children default-open">
+									<li class="page_item page-item-155 menu-item"><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/the-codex/creating-a-new-document/">Creating a New Document</a></li>
+									<li class="page_item page-item-157 menu-item"><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/the-codex/creating-a-user-page/">Creating a User Page</a></li>
+									<li class="page_item page-item-159 menu-item"><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/the-codex/getting-started/">Getting Started</a></li>
+									<li class="page_item page-item-165 menu-item"><a href="https://make.wordpress.org/docs/handbook/tutorials-and-guides/the-codex/mediawiki-formatting/">MediaWiki Formatting</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </aside>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
