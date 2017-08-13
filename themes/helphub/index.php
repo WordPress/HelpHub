d9ce2a7518ff45bb722059d86d54d0899f66ff99
Milana <milanazzap@gmail.com>
2017-08-13 22:47:26 +0200

    Removed comments template from frontpage and commented out for posts (in case we want them back sometime in the future).

--- themes/helphub/index.php
+++ themes/helphub/index.php
@@ -23,10 +23,12 @@ get_header(); ?>
 
 			the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
+			// We don't want comments at this point
+			/*
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+			*/
 
 		endwhile; // End of the loop.
 		?>
