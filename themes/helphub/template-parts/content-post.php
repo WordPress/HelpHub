7192e29b85a6a7ee5bdd6ebf6c994360a5d525ea
Milana <milanazzap@gmail.com>
2017-09-07 00:26:02 +0200

    Post typography.

--- themes/helphub/template-parts/content-post.php
+++ themes/helphub/template-parts/content-post.php
@@ -9,40 +9,42 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'handbook' ) ); ?>>
+<div class="o2-posts">
+	<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'handbook' ) ); ?>>
 
+		<div class="o2-post">
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<div class="entry-content">
+				<?php
+					the_content();
 
-			<span class="read-time">
-				<?php helphub_wrap_the_read_time(); ?>
-			</span>
-		</header><!-- .entry-header -->
-		<?php
-		the_content();
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'helphub' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'helphub' ),
-			'after'  => '</div>',
-		) );
-		?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'helphub' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	</div><!-- .entry-content -->
+			<footer class="entry-footer">
+				<?php
+					edit_post_link(
+						sprintf(
+							/* translators: %s: Name of current post */
+							esc_html__( 'Edit %s', 'helphub' ),
+							the_title( '<span class="screen-reader-text">"', '"</span>', false )
+						),
+						'<span class="edit-link">',
+						'</span>'
+					);
+				?>
+				<span class="read-time">
+					<?php helphub_wrap_the_read_time(); ?>
+				</span>
+			</footer><!-- .entry-footer -->
+		</div><!-- .o2-post -->
 
-
-
-</article><!-- #post-## -->
+	</article><!-- #post-## -->
+</div><!-- .o2-posts -->
