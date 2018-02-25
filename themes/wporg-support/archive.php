6e89fb4e907b85d21b7de9175b1dad01bcbff66d
Clorith <bg3erl42>
2018-02-25 17:24:13 +0100

    Generic archive template for anything not explicitly defined.

---
+++ themes/wporg-support/archive.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The catchall archive template.
+ *
+ * If no specific archive layout is defined, we'll go with
+ * a generic simplistic one, like this, just to actually
+ * be able to show some content.
+ *
+ * @package WPBBP
+ */
+
+get_header(); ?>
+
+
+	<main id="main" class="site-main" role="main">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+		?>
+
+		<?php get_template_part( 'template-parts/content', 'archive' ); ?>
+
+		<?php endwhile; ?>
+
+		<div class="archive-pagination">
+			<?php posts_nav_link(); ?>
+		</div>
+
+	</main>
+
+
+<?php
+get_footer();
