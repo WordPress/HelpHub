326db1b7d986c2362f1a7224c6479fc3e09834c9
Carl Alberto <cralberto11@gmail.com>
2018-03-19 15:04:47 +0800

    added an initial version of the custom role fro helphub

---
+++ plugins/support-helphub/inc/helphub-customroles/helphub-custom-roles.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: HelpHub Custom Roles
+ * Version: 1.0
+ * Plugin URI: https://carl.alber2.com/
+ * Description: This adds HelpHub custom roles.
+ * Author: Carl Alberto
+ * Author URI: https://carl.alber2.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: helphub-custom-roles
+ * Domain Path: /lang/
+ *
+ * @package HelpHub
+ * @author Carl Alberto
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load plugin class files.
+require_once 'includes/class-helphub-custom-roles.php';
+
+/**
+ * Returns the main instance of HelpHub_Custom_Roles to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object HelpHub_Custom_Roles
+ */
+function helphub_custom_roles() {
+	$instance = HelpHub_Custom_Roles::instance( __FILE__, '1.0.0' );
+	return $instance;
+}
+
+helphub_custom_roles();
