e85cd9be87392fd87d6d6521f91af42b27f4375a
Stephen Edgar <stephen@netweb.com.au>
2018-06-01 13:18:04 +1000

    Support Theme: Update build tools per w.original (#222)
    See:
    * https://meta.trac.wordpress.org/changeset/7254
    * https://meta.trac.wordpress.org/changeset/7255

--- themes/wporg-support/Gruntfile.js
+++ themes/wporg-support/Gruntfile.js
@@ -1,37 +1,73 @@
 module.exports = function(grunt) {
+	var autoprefixer = require( 'autoprefixer' );
+	var scsssyntax = require( 'postcss-scss' );
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON( 'package.json' ),
 		sass: {
 			dist: {
 				files: {
-					'style.css' : 'sass/style.scss'
+					'style.css': 'sass/style.scss',
 				},
 				options: {
 					outputStyle: 'expanded',
 					indentType: 'tab',
 					indentWidth: 1,
-					sourceMap: true
-				}
+					sourceMap: true,
+				},
+			},
+		},
+		stylelint: {
+			scss: {
+				options: {
+					syntax: 'scss'
+				},
+				expand: true,
+				src: [
+					'sass/**/*.scss',
+					'!sass/_normalize.scss',
+					'!sass/mixins/_breakpoint.scss',
+					'!sass/mixins/_modular-scale.scss',
+				],
 			}
+		},
+		postcss: {
+			options: {
+				map: false,
+				processors: [
+					autoprefixer({
+						browsers: [ 'extends @wordpress/browserslist-config' ],
+						cascade: false,
+					}),
+				],
+				syntax: scsssyntax,
+				failOnError: true,
+			},
+			dist: {
+				expand: true,
+				src: [ 'sass/**/*.scss' ],
+			},
 		},
 		rtlcss: {
 			dist: {
 				files: {
-					'style-rtl.css' : 'style.css'
-				}
-			}
+					'style-rtl.css': 'style.css',
+				},
+			},
 		},
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass', 'rtlcss']
-			}
-		}
+				tasks: ['stylelint', 'sass', 'rtlcss'],
+			},
+		},
 	});
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-rtlcss' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
+	grunt.loadNpmTasks( 'grunt-rtlcss' );
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
-	grunt.registerTask( 'build', [ 'sass', 'rtlcss' ] );
-	grunt.registerTask( 'default', [ 'build' ] );
+	grunt.registerTask( 'build', [ 'postcss', 'sass', 'rtlcss' ]);
+	grunt.registerTask( 'default', [ 'build' ]);
 };
